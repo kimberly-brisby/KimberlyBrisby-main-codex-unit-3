a1540c158d0740d5cbf796a578c0510ad19a49d9
Kimberly Brisby <cappykim7@yahoo.com>
2026-06-16 15:44:22 -0500

    completed 4 error was displayed

--- lesson-01-nodejs/level-04-error-handling/script.js
+++ lesson-01-nodejs/level-04-error-handling/script.js
@@ -1,1 +1,8 @@
 console.log("Attempting to access browser features...");
+
+try {
+    localStorage.setItem("usrename","kim123");
+    const pTag = document.querySelector("p");
+} catch (error) {
+    console.log(error);
+}
