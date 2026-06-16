be29e21477dd16a40d9071bb49cec2a74876a5c9
Kimberly Brisby <cappykim7@yahoo.com>
2026-06-16 16:18:31 -0500

    level 7 is done then I added to show the numbers

---
+++ lesson-01-nodejs/level-07-array-traversal/script.js
@@ -0,0 +1,5 @@
+const colors = ["pink","blue","red", "purple","green","midnight","seafoam","yellow","cyan","grey"];
+
+for (let i = 0; i < colors.length; i++){
+    console.log(`${i+1} my value is ${colors[i]}.`)
+}
