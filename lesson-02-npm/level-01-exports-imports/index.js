9cc6800311d714575f37233783b6393fa36afcfb
Kimberly Brisby <cappykim7@yahoo.com>
2026-06-17 08:02:27 -0500

    level one is complete and i explained in index

--- lesson-02-npm/level-01-exports-imports/index.js
+++ lesson-02-npm/level-01-exports-imports/index.js
@@ -6,3 +6,5 @@ import names from "./names.js";
 greet("Frontend Developer");
 stats(3, 7);
 showNames(names);
+
+console.log("Importing and exporting code seperates the code into smaller files and can be used for a certain part of the code making things look better organized and easier to read.")
