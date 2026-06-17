d26e2c76e7130e9c2b91395923ab238881c11cff
Kimberly Brisby <cappykim7@yahoo.com>
2026-06-17 09:00:33 -0500

    followed the instructions for level 4 in the terminal messages popped up I think i did everything right

--- lesson-02-npm/level-04-node-modules/script.js
+++ lesson-02-npm/level-04-node-modules/script.js
@@ -1,1 +1,11 @@
-import {} from "node:os";
+import {cpus} from "node:os";
+import {totalmem} from "node:os";
+import {freemem} from "node:os";
+import { machine } from "node:os";
+import { platform } from "node:os";
+
+console.log(cpus);
+console.log(totalmem);
+console.log(freemem);
+console.log(machine);
+console.log(platform);
