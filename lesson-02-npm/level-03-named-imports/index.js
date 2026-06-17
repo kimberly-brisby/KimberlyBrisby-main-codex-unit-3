c00858f2aaf07e03460cb925db5b2f19bf854c80
Kimberly Brisby <cappykim7@yahoo.com>
2026-06-17 08:52:25 -0500

    level 3 followed instructions but terminal is still not cooperating with me

---
+++ lesson-02-npm/level-03-named-imports/index.js
@@ -0,0 +1,5 @@
+import {message1} from "./messages.js";
+console.log(message1);
+import {message2} from "./message.js";
+
+console.log(message2);
---
+++ lesson-02-npm/level-03-named-imports/messages.js
@@ -0,0 +1,2 @@
+export const message1 =  "Hello World!";
+export const message2 = "Named exports must be on the same line and most times the file name matches the varible and function being called.";
