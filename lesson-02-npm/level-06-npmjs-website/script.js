7d64cb2fd83fd474a85c786dfd4538a8238a68a2
Kimberly Brisby <cappykim7@yahoo.com>
2026-06-17 21:04:13 -0500

    finished 2-6 and answered the questions

---
+++ lesson-02-npm/level-06-npmjs-website/script.js
@@ -0,0 +1,3 @@
+const messages = ["The NPMJS website is a site that is can be used as reusable code form.","I looked up elephants and it showed a lot of code that I could not understand and it was published 3 years ago."];
+
+console.log(messages);
