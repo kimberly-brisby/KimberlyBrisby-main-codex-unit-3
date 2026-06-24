7b46a66225e52e76c9efea119b7c15311c38eb17
Kimberly Brisby <cappykim7@yahoo.com>
2026-06-24 14:01:01 -0500

    added react template and changed name for 3-8 and 3-9

---
+++ lesson-03-react/level-08-react-page/react-page/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return <main>
+    <h1>React Template</h1>
+    <p>In order to create a react tempete you first need to create a vite file in the terminal. Then you select what type it will be in this case react and Javascript. Once you hit enter let the all the code automatically install. Select yes to complete and you have a react template</p>
+  </main>;
+}
+
+export default App
