76d254c8f05f4c04e8c25f46122df94323b1551d
Kimberly Brisby <cappykim7@yahoo.com>
2026-06-25 06:39:55 -0500

    had to re-do 3-7 deleted file by accident

---
+++ lesson-03-react/level-07-react-template/react-template/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <mian>
+      <h1>React Template</h1>
+      <p>In order to create  react template you have to first start a vite project. Then you will follow the instrucion in the terminal to select the type of project like react and javascript. Allow the porgram to auto fill the folders and files. Once that is finsied you can start the coding process.</p>
+    </mian>
+  );
+}
+
+export default App
