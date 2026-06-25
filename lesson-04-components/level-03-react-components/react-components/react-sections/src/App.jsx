f7a9d811cea30ecc91b38aae96698397b0401ead
Kimberly Brisby <cappykim7@yahoo.com>
2026-06-24 23:00:49 -0500

    have to come to 4-3 I do not know what I did but browers is not working and have double the files

---
+++ lesson-04-components/level-03-react-components/react-components/react-sections/src/App.jsx
@@ -0,0 +1,16 @@
+import { Header } from "./components/Header.jsx";
+import { Main } from "./components/Main.jsx";
+import { Footer } from "./components/Footer.jsx";
+import './App.css'
+
+function App() {
+  return(
+   <>
+    <Header />
+    <Main />
+    <Footer />
+   </>
+  );
+}
+
+export default App
