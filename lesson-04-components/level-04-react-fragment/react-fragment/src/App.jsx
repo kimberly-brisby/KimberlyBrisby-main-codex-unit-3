99c75be078fd573c9f41b66c9e8247048e656480
Kimberly Brisby <cappykim7@yahoo.com>
2026-06-25 08:10:43 -0500

    4-4 is completed

---
+++ lesson-04-components/level-04-react-fragment/react-fragment/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+   <>
+   <header><h1>React Fragment</h1></header>
+   <main><p>React components hold 1 HTML tag, however that can tag can hold multiple tags. These tags appear side by side and are invisible HTML tags.</p></main>
+   <footer>Kimberly Brisby</footer>
+   </>
+  );
+}
+
+export default App
