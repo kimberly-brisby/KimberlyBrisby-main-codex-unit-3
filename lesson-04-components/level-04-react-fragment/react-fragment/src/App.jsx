719d7ca6bb486a8cc25ed5dbdc0337baa220976c
Kimberly Brisby <cappykim7@yahoo.com>
2026-06-25 15:59:22 -0500

    redid 4-4 lost code some how

--- lesson-04-components/level-04-react-fragment/react-fragment/src/App.jsx
+++ lesson-04-components/level-04-react-fragment/react-fragment/src/App.jsx
@@ -5,15 +5,15 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
- 
+  const [count, setCount] = useState(0)
 
   return (
-   <>
-   <header><h1>React Fragment</h1></header>
-   <main><p>React components hold 1 HTML tag, however that can tag can hold multiple tags. These tags appear side by side and are invisible HTML tags.</p></main>
-   <footer>Kimberly Brisby</footer>
-   </>
-  );
+    <>
+      <header><h1>React Fragment</h1></header>
+      <main><p>React fragments return 1 Html but can be housed in an empty react fragment tag. </p></main>
+      <footer>Kimberly Brisby</footer>
+    </>
+  )
 }
 
 export default App
