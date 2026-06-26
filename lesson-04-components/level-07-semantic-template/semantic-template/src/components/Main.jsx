5f53b2df4dbb6381cd7fd7fe67d4ded4c12d73d6
Kimberly Brisby <cappykim7@yahoo.com>
2026-06-25 20:12:25 -0500

    4-7 is finished. a little challanging but go it done.

---
+++ lesson-04-components/level-07-semantic-template/semantic-template/src/components/Main.jsx
@@ -0,0 +1,5 @@
+export function Main() {
+    return (
+        <main><p>The body tag is selected as a react root and then by using the components folder you can render code like a function.</p></main>
+    );
+}
