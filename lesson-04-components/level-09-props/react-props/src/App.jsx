3335e9e56d24af3ec810fd48628bbc03e22953d5
Kimberly Brisby <cappykim7@yahoo.com>
2026-06-26 19:26:26 -0500

    4-9 is done

---
+++ lesson-04-components/level-09-props/react-props/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import { Header } from "./components/Header";
+import { Main } from "./components/Main";
+import { Footer } from "./components/Footer";
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <Header title =
+     "React Props"/>
+     <Main message ="The way to use props is to add the word props in () after function name as a object. Then you add a function to the app.jsx and call in the corresponding file in {}. "/>
+     <Footer closing="Kimberly Brisby"/>
+    </>
+  );
+}
+
+export default App
---
+++ lesson-04-components/level-09-props/react-props/src/components/Main.jsx
@@ -0,0 +1,8 @@
+export function Main(props) {
+    
+    return (
+        <main>
+            <p>{ props.message }</p>
+        </main>
+    );
+}
