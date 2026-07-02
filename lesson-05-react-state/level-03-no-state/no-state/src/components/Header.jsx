7a896db01b342feebae683aebea8d052da765816
Kimberly Brisby <cappykim7@yahoo.com>
2026-07-01 19:21:38 -0500

    5-3 need to come back

---
+++ lesson-05-react-state/level-03-no-state/no-state/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export function Header() {
+   
+    
+    return(
+        <header><h1> Stateless Variables </h1></header>
+    );
+}
---
+++ lesson-05-react-state/level-03-no-state/no-state/src/components/Main.jsx
@@ -0,0 +1,15 @@
+export function Header() {
+   
+    let count = 0;
+    return(
+       
+        <main>
+             function handleClick(event){
+            event.preventDefault()
+        }:
+            <button>Click Counter</button>
+            <p>Count equals</p>
+            <p>A stateless variables doesnot hold value and is reset every time function is called.</p>
+        </main>
+    );
+}
