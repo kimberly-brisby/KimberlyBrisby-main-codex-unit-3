0e3b43350eb2266d290572e0cb9e3e75fc583f2b
Kimberly Brisby <cappykim7@yahoo.com>
2026-07-02 14:32:21 -0500

    2-4 updated

--- lesson-02-npm/level-04-node-modules/script.js
+++ lesson-02-npm/level-04-node-modules/script.js
@@ -1,11 +1,16 @@
-import {cpus} from "node:os";
-import {totalmem} from "node:os";
-import {freemem} from "node:os";
-import { machine } from "node:os";
-import { platform } from "node:os";
+import cpus from "./node:os";
+import totalmem from "./node:os";
+import freemem from "./node:os";
+import  machine  from "./node:os";
+import  platform  from "./node:os";
+import resolve from "./node.path";
+import parse from "./node.path";
 
 console.log(cpus);
 console.log(totalmem);
 console.log(freemem);
 console.log(machine);
 console.log(platform);
+
+console.log(resolve);
+console.log(parse);
