e9d4013e2d3e362f2bb6324d23c7a1559d2f145b
Kimberly Brisby <cappykim7@yahoo.com>
2026-07-02 14:25:11 -0500

    2-3 fixed code

--- lesson-02-npm/level-03-named-imports/index.js
+++ lesson-02-npm/level-03-named-imports/index.js
@@ -1,5 +1,5 @@
-import {message1} from "./messages.js";
+import message1 from "./messages.js";
 console.log(message1);
-import {message2} from "./message.js";
+import message2 from "./message.js";
 
 console.log(message2);
