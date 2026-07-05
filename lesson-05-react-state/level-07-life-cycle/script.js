b402b4fa3a31d9e83cf08360f1897902ec3bdbac
Kimberly Brisby <cappykim7@yahoo.com>
2026-07-04 21:06:35 -0500

    5-7 is complete

---
+++ lesson-05-react-state/level-07-life-cycle/script.js
@@ -0,0 +1,7 @@
+const messages = [
+    "There are 3 phases to the lifecycle of React components; mount, update, and unmount.",
+"The mount component is added to the DOM.",
+"The update component is for the chamges in the DOM.",
+"The unmount component removes it from the DOM."];
+
+console.log(messages);
