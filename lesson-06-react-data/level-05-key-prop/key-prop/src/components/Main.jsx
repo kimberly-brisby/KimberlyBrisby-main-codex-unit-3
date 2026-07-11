455d0857d2cc6abd5ddc9d4616ddf8d2b8672016
Kimberly Brisby <cappykim7@yahoo.com>
2026-07-10 22:23:58 -0500

    6-5 is done

---
+++ lesson-06-react-data/level-05-key-prop/key-prop/src/components/Main.jsx
@@ -0,0 +1,77 @@
+import { useState, useEffect } from "react";
+
+export function Main(){
+    debugger;
+        const [values, setValues] = useState();
+        const [didMount, setDidMount] = useState(false);
+        useEffect(componentDidMount,[]);
+
+    return (
+        <main>
+            <p>The key prop/atribute is a specific ID that keeps track of each element in a React. </p>
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label>Name</label>
+                <input type="text" name="fullName"></input>
+              </div>
+
+              <div>
+                <label>Number</label>
+                <input type="text" name="number"></input>
+              </div>
+
+              <div>
+                <label>Animal</label>
+                <input type="text" name="animal"></input>
+              </div>
+
+              <div>
+                <label>Place</label>
+                <input type="text" name="place"></input>
+              </div>
+
+              <div>
+                <label>Color</label>
+                <input type="text" name="color"></input>
+              </div>
+              <submit type="submit">Submit</submit>
+            </form>
+            <output>{values}</output>
+            
+        </main>
+    );
+
+    function componentDidMount(){
+        setDidMount(true);
+        
+    }
+    function handleSubmit(event){
+        debugger;
+        event.preventDefault();
+        const form = event.target;
+        const formInputs = [
+            {label: name, value: form.elements.name.value},
+            {label: number, value: form.elements.name.value},
+            {label: animal, value: form.elements.animal.value},
+            {label: place, value: form.elements.place.value},
+            {label:color, value: form.elements.color.value}
+        ];
+        const details = formInputs.map(toDetails);
+        setValues(details);
+
+
+    }
+
+    function toDetails(formInput){
+        debugger;
+        const key = index + formInput.label;
+    
+        const details = <details key={key}>
+            <summary>{formInput.type}</summary>
+
+        </details>
+        
+    }
+
+    return details;
+}
