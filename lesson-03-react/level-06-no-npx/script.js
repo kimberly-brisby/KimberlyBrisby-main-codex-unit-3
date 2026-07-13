ec08476e64f37c4748a81ffebaf54d50fecd4bcc
Kimberly Brisby <cappykim7@yahoo.com>
2026-07-13 14:22:53 -0500

    3-6 had to redo

--- lesson-03-react/level-06-no-npx/script.js
+++ lesson-03-react/level-06-no-npx/script.js
@@ -1,1 +1,4 @@
-console.log("Npx is required for one-off commands like cowsay and is not required for everday day tools like react.")
+const message1 = ["npx cowsay is npx required?"];
+const message2 = ["cowsay is npx required?"];
+
+console.log("Npx is required for one-off commands like cowsay and is not required for everday day tools like react.");
