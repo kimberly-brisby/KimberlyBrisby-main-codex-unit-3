fed457c766f86f25d90571aedc606ac7d1bebe9b
Kimberly Brisby <cappykim7@yahoo.com>
2026-07-29 20:32:10 -0500

    started 7-4

---
+++ lesson-07-database/level-04-spa-template/spa-template/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export function Footer(){
+    return(
+        <Footer>
+            <h6>Kimberly Brisby</h6>
+        </Footer>
+    );
+}
---
+++ lesson-07-database/level-04-spa-template/spa-template/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export function Header(){
+    return(
+        <Header>
+            <h1>React Spa Template</h1>
+        </Header>
+    );
+}
